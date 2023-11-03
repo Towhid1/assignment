0e9b575488ae255eca9b7cbeca34466b9e6fce89
Towhid1 <nurulaktertowhid@gmail.com>
2023-11-03 17:11:32 +0600

    test code

--- test_api.py
+++ test_api.py
@@ -1,17 +1,34 @@
 import requests
 from time import time
+
 # ============================================
 # Task 1 test
 # ============================================
-url = "http://127.0.0.1:8000/coolest_ten"
-
+BASE_URL = "http://127.0.0.1:8000"  # Replace with the actual address of your FastAPI server
+run_n = 10
 start = time()
 try:
-    response = requests.get(url)
+    for i in range(run_n):
+        response = requests.get(f"{BASE_URL}/coolest_ten")
     response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
-    data = response.json()
-    print(data)
+    print(f"api response : {response.json()}")
 except requests.exceptions.RequestException as e:
     print(f"Error: {e}")
 execution_time = time() - start
-print(f"execution_time : {execution_time} sec")
+print(f"\n>> 1st task avg execution_time : {execution_time/run_n} sec")
+
+# ============================================
+# Task 2 test
+# ============================================
+start = time()
+date_str = "2023-11-03"  # Replace with the desired date
+try:
+    for i in range(run_n):
+        response = requests.get(f"{BASE_URL}/predict/{date_str}")
+    response.raise_for_status()
+    print(f"api response : {response.json()}")
+
+except requests.RequestException as e:
+    print(f"Request error: {e}")
+execution_time = time() - start
+print(f"\n>> 2nd task execution_time : {execution_time/run_n} sec")
