7695c1dcd1fc05e4a09ea00bcad58fe45cb3b2dc
Towhid1 <nurulaktertowhid@gmail.com>
2023-11-03 15:38:59 +0600

    api init

---
+++ api.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import pandas as pd
+from download_data import AVG_TEMP_DIR
+
+df = pd.read_csv(AVG_TEMP_DIR)
+
+app = FastAPI()
+
+
+@app.get("/coolest_ten")
+async def coolest_ten():
+    try:
+        result_json = df.to_json(orient="records")
+        return {"data": result_json}
+    except FileNotFoundError:
+        return {"error": "CSV file not found."}
+    except pd.errors.EmptyDataError:
+        return {"error": "CSV file is empty or not valid."}
+    except Exception as e:
+        return {"error": str(e)}
